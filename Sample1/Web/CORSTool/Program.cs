ff6b79c85d784281dc64912cc0ed0d8a82d9c66c
rolandkru <roland.krummenacher@hotmail.de>
2014-04-13 13:01:49 +0200

    Web Sample

---
+++ Sample1/Web/CORSTool/Program.cs
@@ -0,0 +1,48 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Selectron Systems AG">
+//   Copyright (c) Selectron Systems AG. All rights reserved.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+namespace CORSTool
+{
+    using System;
+    using System.Collections.Generic;
+
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Shared.Protocol;
+
+    /// <summary>
+    /// The program.
+    /// </summary>
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                var storageAccount = CloudStorageAccount.Parse("DefaultEndpointsProtocol=https;AccountName=rwwa;AccountKey=vyMfaSPxURHXZaIhhFJQRg5ZLEN6qDj4yU78r3oeOH+pZzdcf4S86QvGAsB6L8JaPti9qJbB929hy1Y9hipFmw==");
+                var blobClient = storageAccount.CreateCloudBlobClient();
+                var serviceProperties = blobClient.GetServiceProperties();
+
+                serviceProperties.Cors.CorsRules.Clear();
+
+                serviceProperties.Cors.CorsRules.Add(
+                new CorsRule
+                {
+                    AllowedHeaders = new List<string> { "accept", "Cache-Control", "Origin", "Referer", "User-Agent", "x-ms-blob-type" }, 
+                    AllowedMethods = CorsHttpMethods.Put | CorsHttpMethods.Head,
+                    AllowedOrigins = new List<string> { "*" },            ////This is the URL of our application.                                                 
+                    MaxAgeInSeconds = 1 * 60 * 60,                                              ////Let the browswer cache it for an hour
+                });
+
+                blobClient.SetServiceProperties(serviceProperties);
+            }
+            catch (Exception exp)
+            {
+                Console.WriteLine(exp);
+            }
+
+            Console.ReadLine();
+        }
+    }
+}
